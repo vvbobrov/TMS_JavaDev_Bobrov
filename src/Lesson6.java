ab4930aa9d185f8fcb4cc8a0a314efba187078c6
bobrik <@>
2026-03-16 19:56:55 +0300

    Lesson6.java: added class "vehicle"

--- src/Lesson6.java
+++ src/Lesson6.java
@@ -2,4 +2,15 @@ public class Lesson6 {
     public static void main (String[] args) {
 
     }
+
+    public class Vehicle {
+        String brand;
+        String model;
+        int vehiclesNumber;
+        String bodyForm;
+        int engineHorsePower;
+        String color;
+        String owner;
+    }
+
 }
